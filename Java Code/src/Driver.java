bf6f475c8a035f7e0150bdfec5b7be9f5a9194a6
Samir Anghan <samir.anghan@gmail.com>
2019-07-27 17:17:02 -0400

    [Code] Added main driver class.

---
+++ Java Code/src/Driver.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("=============== :: CALCULATOR :: ===============");
+		System.out.println("\nSelect use case:\n");
+		System.out.println("Press 1 : To calculate the value of the silver ratio number up to given certain decimal places.");
+		System.out.println("Press 2 : To calculate an area of a regular octagon with given side length");
+		System.out.println("Press 3 : To add a certain number to the value of the silver ratio number:");
+		System.out.println("Press 4 : To subtract a certain number from the value of the silver ratio number:");
+		System.out.println("Press 5 : To multiply a certain number with the value of the silver ratio number");
+		System.out.println("Press 6 : To divide a certain number by the value of the silver ratio number");
+		System.out.println("Press 7 : To divide the value of the silver ratio number by a certain number,");
+		
+		int userChoice = in.nextInt();
+		
+		switch (userChoice) {
+		case 1:
+			calculateSilverRatio();
+			break;
+			
+		case 2:
+			break;
+		
+		case 3:
+			break;
+			
+		case 4:
+			break;
+			
+		case 5:
+			break;
+			
+		case 6:
+			break;
+			
+		case 7:
+			break;
+			
+		default:
+			System.out.println("\nINVALID CHOICE !!! \nRun again to use the library management system.");
+			break;
+		}
+
+	}
+
+	private static void calculateSilverRatio() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
